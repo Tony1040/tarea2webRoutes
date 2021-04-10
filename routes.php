27f5810bfb12baf368d2d1d1ef124565de8f5537
Anthony Ramírez López <arleyy1004@hotmail.com>
2021-04-10 16:10:06 -0600

    number changes

--- routes.php
+++ routes.php
@@ -3,7 +3,7 @@
     Route::get('/', function () {return view('home');});
 
 
-    Route::get('libro1', function () {return view('libro1');});
+    Route::get('libro', function () {return view('libro');});
     Route::get('libro2', function () {return view('libro2');});
     Route::get('libro3', function () {return view('libro3');});
     Route::get('libro4', function () {return view('libro4');});
