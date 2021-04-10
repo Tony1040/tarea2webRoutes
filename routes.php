cc0ad8e1ce8ff23a53ed5308404c083e94f520d9
Anthony Ramírez López <arleyy1004@hotmail.com>
2021-04-10 14:52:11 -0600

    routing changes

--- routes.php
+++ routes.php
@@ -3,10 +3,10 @@
     Route::get('/', function () {return view('home');});
 
 
-    Route::get('/libro1', function () {return view('libro1');});
-    Route::get('/libro2', function () {return view('libro2');});
-    Route::get('/libro3', function () {return view('libro3');});
-    Route::get('/libro4', function () {return view('libro4');});
+    Route::get('libro1', function () {return view('libro1');});
+    Route::get('libro2', function () {return view('libro2');});
+    Route::get('libro3', function () {return view('libro3');});
+    Route::get('libro4', function () {return view('libro4');});
 
     Route::dispatch();
 ?>
